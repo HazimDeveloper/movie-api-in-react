d619781564008d6444bc26e1cf48604e6707779c
HazimDeveloper <hazimdeveloper@gmail.com>
2023-03-22 09:16:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+
+import { Container,Row,Col,Card,Form,InputGroup } from 'react-bootstrap';
+import React , {useEffect,useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [movies,setMovies] = useState([])
+
+    const searchMovie = (q) => {
+    
+    axios.get(`${process.env.REACT_APP_BASE_URL}/search/movie?query=${q}&api_key=${process.env.REACT_APP_API_KEY}`)
+      .then((response) => {
+
+        if(q.length > 3){
+
+          
+          setMovies(response.data.results);
+        }
+      }); 
+
+    }
+    useEffect(() =>{
+      axios.get(`${process.env.REACT_APP_BASE_URL}/movie/popular`,{
+        params: {
+          api_key : process.env.REACT_APP_API_KEY
+
+        }
+      }).then((response) => {
+        setMovies(response.data.results)
+      })
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div className=' mx-auto bg-warning '>    
+     <Container >
+        <Row> 
+
+      <h2 className='text-center mt-5 '>Welcome To Hakas Movie</h2>
+      <h5 className='text-center mt-2'>Made With ❤️️  By Muhammad Hazim </h5>
+      <InputGroup className="mb-3 mt-3">
+        <Form.Control
+          placeholder="Search Movie You Love ...."
+        onChange={({target}) => { searchMovie(target.value) }}
+        />
+      </InputGroup>
+      {movies.map((result,index) => {
+      
+        if(result === 0 ){
+          return "Movie Not Found";
+        } 
+        return(
+<Col xl={3} md={4} sm={6} key={index} className='d-flex justify-content-center align-items-center mt-2 mb-3'>
+<Card style={{width:'100%', height:'100%'}} className='bg-dark text-white border ' >
+
+  <Card.Img variant="top" src={result.poster_path == null ? process.env.PUBLIC_URL + '/imgnotfound.png'  : process.env.REACT_APP_IMAGE + result.poster_path} />
+      <Card.Body>
+        <Card.Title className="text-center mb-2">{result.title}</Card.Title>
+        <Card.Text>
+
+          <div className='mt-3 mb-1'> Language :  {result.original_language}</div>
+          <div>Release Date : {result.release_date}</div>
+          <div>⭐️{result.vote_average}</div>
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+</Col>
+        )
+      }) }
+                
+      </Row>
+      </Container>
+    
+      </div>
+      
+  </>
   );
-}
+
+  }
+
 
 export default App;
